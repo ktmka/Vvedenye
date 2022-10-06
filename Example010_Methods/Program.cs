44a6b57da92f2e402bdb90e493e4c79af37674f2
ktmka <tomatimoto@gmail.com>
2022-10-06 17:01:39 +0500

    add Example010_Methods

---
+++ Example010_Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача на поиск элемента в массиве
+int[] array = {4, 11, 21, 4, 31, 31, 15, 61, 17, 18, 19};
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find) 
+    {   Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
